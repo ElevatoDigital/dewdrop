5f6f2590b50c0d830841b48bb4b7718902d1d4a2
Brad Griffith <griffbrad@gmail.com>
2013-03-14 15:48:12 -0500

    Docs for the CLI RendererInterface.

--- Dewdrop/Cli/Renderer/RendererInterface.php
+++ Dewdrop/Cli/Renderer/RendererInterface.php
@@ -2,17 +2,60 @@
 
 namespace Dewdrop\Cli\Renderer;
 
+/**
+ * The interface CLI renderers must implement.  Markdown is used by
+ * default, but alternate renders could be used to render directly to
+ * HTML, etc.
+ *
+ * @package Dewdrop
+ */
 interface RendererInterface
 {
+    /**
+     * Display the primary title for the output.
+     *
+     * @param string
+     * @returns RendererInterface
+     */
     public function title($title);
 
+    /**
+     * Display a subhead, or 2nd-level header.
+     *
+     * @param string
+     * @returns RendererInterface
+     */
     public function subhead($subhead);
 
+    /**
+     * Display a single line or block of text.
+     *
+     * @param string
+     * @returns RendererInterface
+     */
     public function text($text);
 
+    /**
+     * Display a table.  The supplied array should have the row title as
+     * the keys and the descriptions as the array values.
+     *
+     * @param array
+     * @returns RendererInterface
+     */
     public function table(array $rows);
 
+    /**
+     * Display an error message.
+     *
+     * @param string
+     * @returns RendererInterface
+     */
     public function error($error);
 
+    /**
+     * Display a newline/line break.
+     *
+     * @returns RendererInterface
+     */
     public function newline();
 }
