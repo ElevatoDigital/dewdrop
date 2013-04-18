72f693b54dc0c84ba38fd11915afa2fc73b68a26
Brad Griffith <griffbrad@gmail.com>
2013-04-18 12:20:02 -0500

    WpSelect tests

---
+++ tests/Dewdrop/View/Helper/WpSelectTest.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace Dewdrop\View\Helper;
+
+use Dewdrop\View\View;
+use PHPUnit_Framework_TestCase;
+use Zend\Dom\Query as DomQuery;
+
+class WpSelectTest extends PHPUnit_Framework_TestCase
+{
+    private $view;
+
+    public function setUp()
+    {
+        $this->view = new View();
+    }
+
+    public function testCanRenderSelectWithExplicitlyPassedArguments()
+    {
+        $out = $this->view->wpSelect(
+            'my_select',
+            array(
+                1 => 'Option 1',
+                2 => 'Option 2'
+            ),
+            1
+        );
+
+        $this->assertMatchesDomQuery('select[name="my_select"]', $out);
+        $this->assertMatchesDomQuery('option[value="1"]', $out);
+        $this->assertMatchesDomQuery('option[value="2"]', $out);
+
+        $results = $this->queryDom('option[selected="selected"]', $out);
+        $this->assertEquals(1, $results[0]->getAttribute('value'));
+    }
+
+    public function testCanRenderSelectWithArgumentsPassedAsArray()
+    {
+        $out = $this->view->wpSelect(
+            array(
+                'name'    => 'my_select',
+                'options' => array(
+                    1 => 'Option 1',
+                    2 => 'Option 2'
+                ),
+                'value'   => 1
+            )
+        );
+
+        $this->assertMatchesDomQuery('select[name="my_select"]', $out);
+        $this->assertMatchesDomQuery('option[value="1"]', $out);
+        $this->assertMatchesDomQuery('option[value="2"]', $out);
+
+        $results = $this->queryDom('option[selected="selected"]', $out);
+        $this->assertEquals(1, $results[0]->getAttribute('value'));
+    }
+
+    public function testIdEqualsNameWhenIdIsntSpecifiedManually()
+    {
+        $out = $this->view->wpSelect(
+            'my_select',
+            array(
+                1 => 'Option 1',
+                2 => 'Option 2'
+            ),
+            1
+        );
+
+        $this->assertMatchesDomQuery('select[name="my_select"]', $out);
+        $this->assertMatchesDomQuery('select[id="my_select"]', $out);
+    }
+
+    public function testCanExplicitlySetIdToBeDifferentThanName()
+    {
+        $out = $this->view->wpSelect(
+            array(
+                'name'    => 'my_select_name',
+                'id'      => 'my_select_id',
+                'options' => array(),
+                'value'   => null
+            )
+        );
+
+        $this->assertMatchesDomQuery('select[name="my_select_name"]', $out);
+        $this->assertMatchesDomQuery('select[id="my_select_id"]', $out);
+    }
+
+    public function testValuesAreCastToStringPriorToComparison()
+    {
+        $out = $this->view->wpSelect(
+            array(
+                'name'    => 'my_select_name',
+                'options' => array(1 => 'Sole Option'),
+                'value'   => '1'
+            )
+        );
+
+        $this->assertMatchesDomQuery('option[selected="selected"][value="1"]', $out);
+    }
+
+    /**
+     * @expectedException \Dewdrop\Exception
+     */
+    public function testPassingAFieldOjbectThrowsAnException()
+    {
+        $db = new \Dewdrop\Db\Adapter\Mock();
+
+        require_once __DIR__ . '/table/DewdropTestFruits.php';
+        $table = new \DewdropTest\DewdropTestFruits($db);
+
+        $this->view->wpSelect($table->field('name'));
+    }
+
+    /**
+     * @expectedException \Dewdrop\Exception
+     */
+    public function testOmittingNameArgumentThrowsException()
+    {
+        $this->view->wpSelect(
+            array(
+                'options' => array(),
+                'value'   => null
+            )
+        );
+    }
+
+    /**
+     * @expectedException \Dewdrop\Exception
+     */
+    public function testOmittingOptionsArgumentThrowsException()
+    {
+        $this->view->wpSelect(
+            array(
+                'name'  => 'test',
+                'value' => null
+            )
+        );
+    }
+
+    /**
+     * @expectedException \Dewdrop\Exception
+     */
+    public function testOmittingValueArgumentThrowsException()
+    {
+        $this->view->wpSelect(
+            array(
+                'options' => array(),
+                'name'    => 'test'
+            )
+        );
+    }
+
+    /**
+     * Assert that the supplied CSS selector matches the supplied HTML.
+     */
+    public function assertMatchesDomQuery($selector, $html)
+    {
+        $results = $this->queryDom($selector, $html);
+
+        $this->assertTrue(
+            count($results) > 0,
+            "The HTML output does not match the DOM query \"{$selector}\"."
+        );
+    }
+
+    public function queryDom($selector, $html)
+    {
+        $query = new DomQuery($html);
+        return $query->execute($selector);
+    }
+}
