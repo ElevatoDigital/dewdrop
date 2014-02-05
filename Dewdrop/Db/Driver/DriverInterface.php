ec1ede9a24b5eed8c67e94518f125d2b41d8efa2
Brad Griffith <bgriffith@deltasys.com>
2014-02-05 10:44:28 -0600

    API docs for driver interface

--- Dewdrop/Db/Driver/DriverInterface.php
+++ Dewdrop/Db/Driver/DriverInterface.php
@@ -10,27 +10,152 @@
 
 namespace Dewdrop\Db\Driver;
 
+/**
+ * Implementing this interface will allow you to integrate other RDBMS vendors
+ * with \Dewdrop\Db\Adapter.  This interface contains methods for the platform-
+ * specific portions of the database API, whereas Adapter has the code shared
+ * amongst all supported platforms.
+ */
 interface DriverInterface
 {
+    /**
+     * Retrieve the raw connection object used by this driver.  For example,
+     * this could be a wpdb object or a PDO connection object.
+     *
+     * @return mixed
+     */
     public function getConnection();
 
+    /**
+     * Fetch all results for the supplied SQL query.
+     *
+     * The SQL query can be a simple string or a Select object.  The bind array
+     * should supply values for all the parameters, either named or numeric, in
+     * the query.  And the fetch mode should match one of these 4 class constants
+     * from \Dewdrop\Db\Adapter: ARRAY_A, ARRAY_N, OBJECT, or OBJECT_K.
+     *
+     * @param mixed $sql
+     * @param array $bind
+     * @param string $fetchMode
+     * @return array
+     */
     public function fetchAll($sql, $bind = array(), $fetchMode = null);
 
+    /**
+     * Fetch a single column of the results from the supplied SQL statement.
+     *
+     * @param string|\Dewdrop\Db\Select $sql
+     * @param array $bind
+     * @return array
+     */
     public function fetchCol($sql, $bind = array());
 
+    /**
+     * Fetch a single scalar value from the results of the supplied SQL
+     * statement.
+     *
+     * @param string|\Dewdrop\Db\Select
+     * @param array $bind
+     * @return mixed
+     */
     public function fetchOne($sql, $bind = array());
 
+    /**
+     * Run the supplied query, binding the supplied data to the statement
+     * prior to execution.
+     *
+     * @param string|\Dewdrop\Db\Select
+     * @param array $bind
+     * @return mixed
+     */
     public function query($sql, $bind = array());
 
+    /**
+     * Get the last insert ID from the driver after performing an insert on a table
+     * with an auto-incrementing primary key.
+     *
+     * @return integer
+     */
     public function lastInsertId();
 
+    /**
+     * Returns the symbol the adapter uses for delimited identifiers.
+     *
+     * @return string
+     */
     public function getQuoteIdentifierSymbol();
 
+    /**
+     * Returns a list of the tables in the database.
+     *
+     * @return array
+     */
     public function listTables();
 
+    /**
+     * Returns an associative array containing all the foreign key relationships
+     * associated with the supplied table.
+     *
+     * The array has the following format:
+     *
+     * <code>
+     * array(
+     *     'column_name' => array(
+     *         'table'  => 'foreign_table',
+     *         'column' => 'foreign_column'
+     *     )
+     * )
+     * </code>
+     *
+     * @param string $tableName
+     * @return array
+     */
     public function listForeignKeyReferences($tableName);
 
+    /**
+     * Returns an associative array containing all the unique constraints on a table.
+     *
+     * The array has the following format:
+     *
+     * <code>
+     * array(
+     *     'key_name' => array(
+     *         sequence_in_index => 'column_name'
+     *     )
+     * )
+     * </code>
+     *
+     * @param string $tableName
+     * @return array
+     */
     public function listUniqueConstraints($tableName);
 
+    /**
+     * Returns the column descriptions for a table.
+     *
+     * The return value is an associative array keyed by the column name,
+     * as returned by the RDBMS.
+     *
+     * The value of each array element is an associative array
+     * with the following keys:
+     *
+     * SCHEMA_NAME      => string; name of database or schema
+     * TABLE_NAME       => string;
+     * COLUMN_NAME      => string; column name
+     * COLUMN_POSITION  => number; ordinal position of column in table
+     * DATA_TYPE        => string; SQL datatype name of column
+     * DEFAULT          => string; default expression of column, null if none
+     * NULLABLE         => boolean; true if column can have nulls
+     * LENGTH           => number; length of CHAR/VARCHAR
+     * SCALE            => number; scale of NUMERIC/DECIMAL
+     * PRECISION        => number; precision of NUMERIC/DECIMAL
+     * UNSIGNED         => boolean; unsigned property of an integer type
+     * PRIMARY          => boolean; true if column is part of the primary key
+     * PRIMARY_POSITION => integer; position of column in primary key
+     * IDENTITY         => integer; true if column is auto-generated with unique values
+     *
+     * @param string $tableName
+     * @return array
+     */
     public function describeTable($tableName);
 }
