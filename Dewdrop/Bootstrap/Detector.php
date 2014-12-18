ce4bc5bff6ad8123264ff9289594166da6b4a7fd
Brad Griffith <bgriffith@deltasys.com>
2014-12-18 09:18:39 -0600

    New CLI command for updated build metadata and associated Pimple resource

--- Dewdrop/Bootstrap/Detector.php
+++ Dewdrop/Bootstrap/Detector.php
@@ -127,6 +127,21 @@ class Detector
             }
         }
 
+        if (!isset($pimple['dewdrop-build'])) {
+            $pimple['dewdrop-build'] = $pimple->share(
+                function () use ($pimple) {
+                    if (defined('APPLICATION_ENV') && 'development' === APPLICATION_ENV) {
+                        return microtime(true);
+                    } else {
+                        /* @var $paths \Dewdrop\Paths */
+                        $paths = $pimple['paths'];
+
+                        return require $paths->getAppRoot() . '/dewdrop-build.php';
+                    }
+                }
+            );
+        }
+
         if (!isset($pimple['session'])) {
             /* @var $paths Paths */
             $paths = $pimple['paths'];
