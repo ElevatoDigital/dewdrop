3cad58f70268f076dd2806dd1f631b7f65b70492
Brad Griffith <bgriffith@deltasys.com>
2015-06-19 16:46:24 -0500

    API docs for URL view helper

--- Dewdrop/View/Helper/Url.php
+++ Dewdrop/View/Helper/Url.php
@@ -1,11 +1,30 @@
 <?php
 
+/**
+ * Dewdrop
+ *
+ * @link      https://github.com/DeltaSystems/dewdrop
+ * @copyright Delta Systems (http://deltasys.com)
+ * @license   https://github.com/DeltaSystems/dewdrop/LICENSE
+ */
+
 namespace Dewdrop\View\Helper;
 
 use Dewdrop\Pimple;
 
+/**
+ * A view helper that will filter any supplied URL through the
+ * url-filter resource in Pimple, if present.  Allows you to manipulate
+ * URLs in your application to prefix them in some way, for example.
+ */
 class Url extends AbstractHelper
 {
+    /**
+     * Filter the supplied URL.
+     *
+     * @param string $url
+     * @return mixed
+     */
     public function direct($url)
     {
         if (!Pimple::hasResource('url-filter')) {
