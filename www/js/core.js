14777ee12b672e8104e5731b03a1ca674965825a
Brad Griffith <bgriffith@deltasys.com>
2015-11-02 17:06:49 -0600

    Only put our WP jQuery shim in place if requirejs is actually available.

--- www/js/core.js
+++ www/js/core.js
@@ -7,18 +7,20 @@
     var scriptTags = document.getElementsByTagName('script'),
         dewdropSrc = scriptTags[scriptTags.length - 1].src,
         bowerUrl   = dewdropSrc.substring(0, dewdropSrc.indexOf('bower_components')) + 'bower_components';
-   
+
     DEWDROP.bowerUrl = function (url) {
-        return bowerUrl + (0 === url.indexOf('/') ? '' : '/') + url;     
+        return bowerUrl + (0 === url.indexOf('/') ? '' : '/') + url;
     };
 
     // Supply pre-loaded jQuery as a requirejs module so we don't have to load it twice in WP
-    define(
-        'jquery',
-        function () {
-            return jQuery;
-        }
-    );
+    if ('undefined' !== typeof define) {
+        define(
+            'jquery',
+            function () {
+                return jQuery;
+            }
+        );
+    }
 
     moment.locale(navigator.language);
 
