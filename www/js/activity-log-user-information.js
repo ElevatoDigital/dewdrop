fc42209955c95144e8487a5aa1d1b9a2bb350ead
Brad Griffith <bgriffith@deltasys.com>
2016-06-10 16:11:53 -0500

    JS for activity log user information popovers.

---
+++ www/js/activity-log-user-information.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+
+    $('.activity-log-user-information').popover({
+        html:      true,
+        container: 'body',
+        placement: 'bottom'
+    });
+}());
