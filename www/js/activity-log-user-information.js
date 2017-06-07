153e74f64b9234e0b57ce752ef94709c996354e1
bravadomizzou <raymondaholland@gmail.com>
2017-06-07 14:25:44 -0500

    Remove Dewdrop js code

--- www/js/activity-log-user-information.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-
-    $('.activity-log-user-information').popover({
-        html:      true,
-        container: 'body',
-        placement: 'bottom'
-    });
-}());
--- www/js/auth-error.js
+++
@@ -1,5 +0,0 @@
-(function () {
-    'use strict';
-
-    $('.panel').velocity('callout.shake');
-}());
