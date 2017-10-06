de98cf734a0c334de892a44c7f84c0fdb51e2d9e
bravadomizzou <raymondaholland@gmail.com>
2017-10-06 13:47:55 -0500

    Add some required node modules

--- www/dist/node/dewdrop.js
+++ www/dist/node/dewdrop.js
@@ -35372,6 +35372,7 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
                     var $popover;
 
                     $input.popover('show');
+
                     $popover = $('[data-input="' + $input.data('input') + '"] .date-wrapper');
 
                     $('[data-input="' + $input.data('input') + '"] a').on('click', function (e) {
