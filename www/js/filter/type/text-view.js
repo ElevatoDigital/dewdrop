eac658453b0fb6022945b4b9366d085d4ed6d691
Darby Felton <darby.felton@deftek.com>
2017-11-13 11:12:30 -0500

    Add support for 'is empty' and 'is not empty' as options for text field filters

--- www/js/filter/type/text-view.js
+++ www/js/filter/type/text-view.js
@@ -7,6 +7,18 @@ define(
 
         return BaseView.extend({
             template: template,
+
+            inputOperators: ['contains', 'not-contains', 'starts-with', 'ends-with'],
+
+            noInputOperators: ['empty', 'not-empty'],
+
+            events: {
+                'change select': 'handleOperatorSelection'
+            },
+
+            postRender: function () {
+                this.handleOperatorSelection();
+            },
             
             updateValues: function () {
                 this.model.set(
@@ -16,6 +28,19 @@ define(
                         value: this.$el.find('input.filter-value').val()
                     }
                 );
+            },
+
+            handleOperatorSelection: function () {
+                var selected = this.$el.find('select').val();
+
+                this.focusInput();
+                this.updateValues();
+
+                if (-1 !== this.inputOperators.indexOf(selected)) {
+                    this.$el.find('input.filter-value').show();
+                } else if (-1 !== this.noInputOperators.indexOf(selected)) {
+                    this.$el.find('input.filter-value').hide();
+                }
             }
         });
     }
