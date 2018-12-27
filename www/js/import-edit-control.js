caa1a9253b1333332ccbfe3beedbc3739098ec2d
Jared Davis <jdavis@deltasys.com>
2018-12-27 12:30:12 -0800

    Add JS logic to add/remove 'required' on fields so Chrome doesn't complain ('An invalid form control with name='people:first_name' is not focusable.') on form submit

--- www/js/import-edit-control.js
+++ www/js/import-edit-control.js
@@ -15,7 +15,9 @@
 
             control.find('.import-mode-input').val('column');
             control.find('.import-column').velocity('slideDown');
+            control.find('.import-column').find('select.form-control').attr('required', true);
             control.find('.import-edit').hide();
+            control.find('.import-edit').find('input.form-control').removeAttr('required');
         }
     );
 
@@ -33,7 +35,9 @@
 
             control.find('.import-mode-input').val('value');
             control.find('.import-edit').velocity('slideDown');
+            control.find('.import-edit').find('input.form-control').attr('required', true);
             control.find('.import-column').hide();
+            control.find('.import-column').find('select.form-control').removeAttr('required');
         }
     );
 
@@ -51,7 +55,9 @@
 
             control.find('.import-mode-input').val('blank');
             control.find('.import-edit').hide();
+            control.find('.import-edit').find('input.form-control').removeAttr('required');
             control.find('.import-column').hide();
+            control.find('.import-column').find('select.form-control').removeAttr('required');
         }
     );
 
@@ -64,12 +70,18 @@
 
             if ('column' === modeInput.val()) {
                 control.find('.import-column').show();
+                control.find('.import-column').find('select.form-control').attr('required', true);
+                control.find('.import-edit').find('input.form-control').removeAttr('required');
                 control.find('.btn-group-import .btn-column').addClass('active');
             } else if ('value' === modeInput.val()) {
                 control.find('.import-edit').show();
+                control.find('.import-edit').find('input.form-control').attr('required', true);
+                control.find('.import-column').find('select.form-control').removeAttr('required');
                 control.find('.btn-group-import .btn-edit').addClass('active');
             } else {
                 control.find('.btn-group-import .btn-blank').addClass('active');
+                control.find('.import-edit').find('input.form-control').removeAttr('required');
+                control.find('.import-column').find('select.form-control').removeAttr('required');
             }
         }
     );
